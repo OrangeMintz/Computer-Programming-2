0499eb23291d5bbbebc6357b9ad22f9c8a957a24
OrangeMintz <101964865+OrangeMintz@users.noreply.github.com>
2022-05-11 02:07:06 +0800

    Add files via upload

---
+++ Activity 1.0 - Crash Course/GACUS_Finding_The_Mean _Of_n_Numbers.c
@@ -0,0 +1,16 @@
+//Djeikuje Nickolai C. Gacus
+//Finding the mean of N numbers
+
+#include <stdio.h>
+int main()
+{
+    int num, sum = 0;
+    do{
+        printf("Enter Integer: ");
+    scanf("%d", &num);
+        sum += num;
+    }
+    while(num != 0.0);
+        printf("\nAverage= %d", sum/2);
+    return 0;
+}
---
+++ Activity 1.0 - Crash Course/GACUS_The_nth_power.c
@@ -0,0 +1,19 @@
+//Djeikuje Nickolai C. Gacus
+//The nth Power
+
+#include <stdio.h>
+
+int main(){
+    int x, y;
+    int cal = 1.0;
+            printf("Enter X value: ");
+        scanf("%d", &x);
+            printf("Enter Y value: ");
+        scanf("%d", &y);
+
+        while (y != 0){
+            cal *= x; --y;
+        }
+        printf("\nResult = %d\n", cal);
+return 0;
+    }
